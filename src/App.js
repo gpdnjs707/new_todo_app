158639d754e8e1774233f3330e816baa84e90278
Hyewon Jeong <hyewon@Hyewons-MacBook-Pro.local>
2020-01-08 13:27:10 +0900

    Form input insert function

--- src/App.js
+++ src/App.js
@@ -7,11 +7,61 @@ import "./App.css";
 
 
 class App extends Component {
+  
+  id = 1;
+
+  state = {
+    input: '',
+    todos: [
+      {id: 0, text: "Make todo App", isDone: false}
+    ]
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleInputInsert = () => {
+    const {input, todos} = this.state
+
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        isDone: false
+      })  
+    });
+  }
+
+  handleKeyPress = (e) => {
+    // 엔터키가 눌리면 Insert func.을 실행한다.
+    if(e.key === 'Enter') {
+      this.handleInputInsert();
+    }
+  }
+
   render() {
+    const {input, todos} = this.state;
+    const {
+      handleInputChange,
+      handleInputInsert,
+      handleKeyPress
+    } = this;
+
     return (
       <div className="wrap">
         <NavContainer></NavContainer>
-        <TodoListContainer form={<Form />}>
+        <TodoListContainer form={(
+          <Form 
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleInputChange}
+            onInsert={handleInputInsert}
+          />
+          )}>
             <TodoItemList />
         </TodoListContainer>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./css/Form.css";
 
-const Form = ({ value, onChange, onKeypress, onClick }) => {
+const Form = ({ value, onChange, onKeyPress, onInsert }) => {
     return (
         <div className="formContainer">
             <input 
                 className="form-control"
                 value={ value }
                 onChange={ onChange }
-                onKeyPress={ onKeypress }
+                onKeyPress={ onKeyPress }
                 placeholder="할일을 입력하세요."
             />
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" onClick={onInsert}>
                 +
             </button>
         </div>
