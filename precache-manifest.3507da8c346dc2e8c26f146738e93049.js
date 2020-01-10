54106705e575a0bb03a24d36ce46c4ae396b1898
Hyewon Jeong <hyewon@Hyewons-MacBook-Pro.local>
2020-01-10 17:48:03 +0900

    Updates

---
+++ precache-manifest.3507da8c346dc2e8c26f146738e93049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8d5124d74148955dde4d7932a09e5f",
+    "url": "/new_todo_app/index.html"
+  },
+  {
+    "revision": "e68841a8a0ff4fcbd615",
+    "url": "/new_todo_app/static/css/main.465f7bd0.chunk.css"
+  },
+  {
+    "revision": "ee96c5febb1d38f44e1f",
+    "url": "/new_todo_app/static/js/2.aa665003.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/new_todo_app/static/js/2.aa665003.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e68841a8a0ff4fcbd615",
+    "url": "/new_todo_app/static/js/main.8e75e1d0.chunk.js"
+  },
+  {
+    "revision": "1c956908f1803562cacc",
+    "url": "/new_todo_app/static/js/runtime-main.d81d3ab9.js"
+  }
+]);
